9e91a1555b8841eb4c768af90af7ffafc79a4bc0
Lionel Caceres <lkcaceres@Lionel-Caceress-MacBook.local>
2015-12-10 20:28:05 -0800

    Initial Commit

---
+++ RecipesGalore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecipesGalore
+//
+//  Created by Lionel Caceres on 12/10/15.
+//  Copyright (c) 2015 Nicholas Caceres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecipesGalore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecipesGalore
+//
+//  Created by Lionel Caceres on 12/10/15.
+//  Copyright (c) 2015 Nicholas Caceres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
